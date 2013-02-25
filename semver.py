6d86be2f6adb5c704b11ec8369fa2bb3c087e8b7
Zachary King <zchrykng@gmail.com>
2013-02-25 14:43:16 -0500

    Restructuring project
    Decided to abandon the wrapper class for now in favor of refining the base
    class for Semantic Version objects. Will eventually gain more features.

--- semver.py
+++ semver.py
@@ -1,44 +1,136 @@
-# class SemVer(object):
+import re
 
 
-    # function stringify (version)
-    # function clean (version)
-    # function valid (version)
-    # function validPackage (version)
-    # function toComparators (range)
-    # function replaceStars (stars)
-    # function replaceXRanges (ranges)
-    # function replaceXRange (version)
-    # function replaceSpermies (version)
-    # function validRange (range)
-    # function maxSatisfying (versions, range)
-    # function satisfies (version, range)
-    # function compare (v1, v2)
-    # function rcompare (v1, v2)
-    # function lt (v1, v2) { return gt(v2, v1); }
-    # function gte (v1, v2) { return !lt(v1, v2); }
-    # function lte (v1, v2) { return !gt(v1, v2); }
-    # function eq (v1, v2) { return gt(v1, v2); }
-    # function neq (v1, v2) { return gt(v1, v2); }
-    # function cmp (v1, c, v2)
-    # function num (v)
-    # function gt (v1, v2)
-    # function inc (version, release)
+class SemVerItem(object):
 
-    # sv_major = r"\s*[v=]*\s*([0-9]+)"
-    # sv_minor = r"\.([0-9]+)"
-    # sv_patch = r"\.([0-9]+)"
-    # sv_build = r"(-[0-9]+-?)?"
-    # sv_tag = r"([a-zA-Z-+][a-zA-Z0-9-\.:]*)?"
-    # semver = sv_major + sv_minor + sv_patch + sv_build + sv_tag
-    # exprComparator = r"^((<|>)?=?)\s*(" + semver + r")$|^$"
-    # xRangePlain = r"[v=]*([0-9]+|x|X|\*)" + r"(?:\.([0-9]+|x|X|\*)" + r"(?:\.([0-9]+|x|X|\*)" + r"([a-zA-Z-][a-zA-Z0-9-\.:]*)?)?)?"
-    # xRange = r"((?:<|>)=?)?\s*" + xRangePlain
-    # exprSpermy = r"(?:~>?)" + xRange
-    # parse = re.compile(r"^\s*" + semver + r"\s*$")
-    # parsePackage = re.compile(r"^\s*([^\/]+)[-@](" + semver + r")\s*$")
-    # parseRange = re.compile(r"^\s*(" + semver + r")\s+-\s+(" + semver + r")\s*$")
-    # validComparator = re.compile(r"^" + exprComparator + r"$")
-    # parseXRange = re.compile(r"^" + xRange + r"$")
-    # parseSpermy = re.compile(r"^" + exprSpermy + r"$")
-    # parseInts = re.compile(r"\d*")
+    regex = re.compile(r'^(?P<major>[0-9]+)'
+                    r'\.(?P<minor>[0-9]+)'
+                    r'\.(?P<patch>[0-9]+)'
+                    r'(\-(?P<prerelease>[0-9A-Za-z]+(\.[0-9A-Za-z]+)*))?'
+                    r'(\+(?P<build>[0-9A-Za-z]+(\.[0-9A-Za-z]+)*))?$')
+
+    # Core methods
+
+    def __init__(self, version):
+        if version == None:
+            self.initialized = False
+        else:
+            self.parse(version)
+            self.initialized = True
+
+    def __bool__(self):
+        return self.initialized
+
+    def __gt__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == 1:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == -1:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == 0:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == 0 and self._compare(other) == 1:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == 0 and self._compare(other) == 1:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, SemVerItem):
+            if self._compare(other) == 1 or self._compare(other) == -1:
+                return True
+            else:
+                return False
+        else:
+            return NotImplemented
+
+    def __str__(self):
+        temp_str = str(self.major) + "." + str(self.minor) + "." + str(self.patch)
+        if self.prerelease != None:
+            temp_str += "-" + str(self.prerelease)
+        if self.build != None:
+            temp_str += "+" + str(self.build)
+        return temp_str
+
+    def __iter__(self):
+        if self.initialized == True:
+            result = [self.major,
+                    self.minor,
+                    self.patch]
+            if self.prerelease != None:
+                result.append(self.prerelease)
+            if self.build != None:
+                result.append(self.build)
+            return iter(result)
+        else:
+            return False
+
+    # Utility functions
+
+    @classmethod
+    def valid(cls, version):
+        if cls.regex.match(version):
+            return True
+        else:
+            return False
+
+    def parse(self, version):
+        match = self.regex.match(version)
+
+        if match is None:
+            raise ValueError('%s is not valid SemVer string' % version)
+
+        info = match.groupdict()
+
+        self.major = int(info['major'])
+        self.minor = int(info['minor'])
+        self.patch = int(info['patch'])
+
+        if info['prerelease'] != None:
+            self.prerelease = info['prerelease']
+        else:
+            self.prerelease = None
+        if info['build'] != None:
+            self.build = info['build']
+        else:
+            self.build = None
+        return True
+
+    def _compare(self, other):
+        for x1, x2 in zip(self, other):
+            if x1 > x2:
+                return 1
+            elif x1 < x2:
+                return -1
+        return 0
