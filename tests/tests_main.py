a1661f50d2d7105296eea03acc129fa6788bafb6
Zachary King <zchrykng@gmail.com>
2013-02-26 11:36:41 -0500

    Removing complete useless code

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -4,96 +4,83 @@ from semver import SemVer
 
 class CompTests(unittest.TestCase):
 
-    def setUp(self):
-        self.item1 = SemVer
-        self.item2 = SemVer
-
     def test_equal(self):
-        self.assertEqual(self.item1("0.0.0-alpha"), self.item1("0.0.0-alpha"))
+        self.assertEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-alpha"))
 
     def test_equal_dot(self):
-        self.assertEqual(self.item1("0.0.0-alpha.12.0"), self.item1("0.0.0-alpha.12.0"))
+        self.assertEqual(SemVer("0.0.0-alpha.12.0"), SemVer("0.0.0-alpha.12.0"))
 
     def test_not_equal(self):
-        self.assertNotEqual(self.item1("0.0.0-beta"), self.item1("0.0.0-alpha"))
+        self.assertNotEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
 
     def test_greater_than(self):
-        self.assertGreater(self.item1("0.0.0-beta"), self.item1("0.0.0-alpha"))
+        self.assertGreater(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
 
     def test_greater_equal(self):
-        self.assertGreaterEqual(self.item1("0.0.0-beta"), self.item1("0.0.0-alpha"))
+        self.assertGreaterEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
 
     def test_greater_equal_equal(self):
-        self.assertGreaterEqual(self.item1("0.0.0-alpha"), self.item1("0.0.0-alpha"))
+        self.assertGreaterEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-alpha"))
 
     def test_less_than(self):
-        self.assertLess(self.item1("0.0.0-alpha"), self.item1("0.0.0-beta"))
+        self.assertLess(SemVer("0.0.0-alpha"), SemVer("0.0.0-beta"))
 
     def test_less_equal(self):
-        self.assertLessEqual(self.item1("0.0.0-alpha"), self.item1("0.0.0-beta"))
+        self.assertLessEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-beta"))
 
     def test_less_equal_equal(self):
-        self.assertLessEqual(self.item1("0.0.0-beta"), self.item1("0.0.0-beta"))
+        self.assertLessEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-beta"))
 
     def test_to_string(self):
-        self.assertEqual(str(self.item1("0.0.0-beta")), "0.0.0-beta")
+        self.assertEqual(str(SemVer("0.0.0-beta")), "0.0.0-beta")
 
 
 class InvalidityTests(unittest.TestCase):
 
-    def setUp(self):
-        self.item = SemVer
-
     def test_invalid_order(self):
-        self.assertFalse(self.item.valid("0.0.0+a400-alpha"))
+        self.assertFalse(SemVer.valid("0.0.0+a400-alpha"))
 
     def test_invalid_num_terms_short(self):
-        self.assertFalse(self.item.valid("0.0"))
+        self.assertFalse(SemVer.valid("0.0"))
 
     def test_invalid_num_terms_long(self):
-        self.assertFalse(self.item.valid("0.0.0.0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0.0.0+a40-alpha"))
 
     def test_invalid_chars_comma(self):
-        self.assertFalse(self.item.valid("0.0,0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0,0+a40-alpha"))
 
     def test_invalid_chars_dash(self):
-        self.assertFalse(self.item.valid("0.0-0.0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0-0.0+a40-alpha"))
 
     def test_invalid_chars_tilda(self):
-        self.assertFalse(self.item.valid("0.0.~+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0.~+a40-alpha"))
 
 
 class ValidityTests(unittest.TestCase):
 
-    def setUp(self):
-        self.item = SemVer
-
     def test_simple(self):
-        self.assertTrue(self.item.valid("0.0.0"))
+        self.assertTrue(SemVer.valid("0.0.0"))
 
     def test_prerelease(self):
-        self.assertTrue(self.item.valid("0.0.0-alpha"))
+        self.assertTrue(SemVer.valid("0.0.0-alpha"))
 
     def test_build(self):
-        self.assertTrue(self.item.valid("0.0.0+a30b"))
+        self.assertTrue(SemVer.valid("0.0.0+a30b"))
 
     def test_prerelease_build(self):
-        self.assertTrue(self.item.valid("0.0.0-beta+a30b"))
+        self.assertTrue(SemVer.valid("0.0.0-beta+a30b"))
 
     def test_invalid_chars_comma(self):
-        self.assertFalse(self.item.valid("0.0.0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0.0+a40-alpha"))
 
     def test_invalid_chars_dash(self):
-        self.assertFalse(self.item.valid("0.0.0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0.0+a40-alpha"))
 
     def test_invalid_chars_tilda(self):
-        self.assertFalse(self.item.valid("0.0.0+a40-alpha"))
+        self.assertFalse(SemVer.valid("0.0.0+a40-alpha"))
 
 
 class RangeChecks(unittest.TestCase):
 
-    def setUp(self):
-        self.item = SemVer
-
     def test_simple_range(self):
-        self.assertTrue(self.item("2.0.0").satisfies("1.0.0 - 3.0.0"))
+        self.assertTrue(SemVer("2.0.0").satisfies("1.0.0 - 3.0.0"))
