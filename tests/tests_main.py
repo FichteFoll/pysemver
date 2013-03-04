82e28411c9e709fc1f7045285232f55be40da089
FichteFoll <fichtefoll2@googlemail.com>
2013-03-04 21:43:04 +0100

    Re-organize some tests (and add more)

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+
 from semver import SemVer
 
 
@@ -6,33 +8,27 @@ class CompTests(unittest.TestCase):
 
     def test_equal(self):
         self.assertEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-alpha"))
-
-    def test_equal_dot(self):
         self.assertEqual(SemVer("0.0.0-alpha.12.0"), SemVer("0.0.0-alpha.12.0"))
 
     def test_not_equal(self):
         self.assertNotEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
 
     def test_greater_than(self):
+        self.assertGreater(SemVer("0.0.1"), SemVer("0.0.0"))
+        # self.assertGreater(SemVer("0.0.1"), SemVer("0.0.1-alpha"))
         self.assertGreater(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
 
     def test_greater_equal(self):
         self.assertGreaterEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-alpha"))
-
-    def test_greater_equal_equal(self):
         self.assertGreaterEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-alpha"))
 
     def test_less_than(self):
         self.assertLess(SemVer("0.0.0-alpha"), SemVer("0.0.0-beta"))
+        # self.assertLess(SemVer("0.0.0-alpha"), SemVer("0.0.0"))
 
     def test_less_equal(self):
         self.assertLessEqual(SemVer("0.0.0-alpha"), SemVer("0.0.0-beta"))
-
-    def test_less_equal_equal(self):
         self.assertLessEqual(SemVer("0.0.0-beta"), SemVer("0.0.0-beta"))
-
-    def test_to_string(self):
-        self.assertEqual(str(SemVer("0.0.0-beta")), "0.0.0-beta")
 
 
 class InvalidityTests(unittest.TestCase):
@@ -46,13 +42,9 @@ class InvalidityTests(unittest.TestCase):
     def test_invalid_num_terms_long(self):
         self.assertFalse(SemVer.valid("0.0.0.0+a40-alpha"))
 
-    def test_invalid_chars_comma(self):
+    def test_invalid_chars(self):
         self.assertFalse(SemVer.valid("0.0,0+a40-alpha"))
-
-    def test_invalid_chars_dash(self):
         self.assertFalse(SemVer.valid("0.0-0.0+a40-alpha"))
-
-    def test_invalid_chars_tilda(self):
         self.assertFalse(SemVer.valid("0.0.~+a40-alpha"))
 
     def test_invalid_prefix(self):
@@ -89,10 +81,6 @@ class ValidityTests(unittest.TestCase):
     def test_constructor(self):
         self.assertTrue(SemVer("0.0.0-a40+alpha"))
         self.assertTrue(SemVer(" 213s 0.0.0-a40+alpha", True))
-        v = SemVer()
-        self.assertFalse(v)
-        v.parse("0.0.0")
-        self.assertTrue(v)
 
 
 class CleanTests(unittest.TestCase):
@@ -110,14 +98,30 @@ class CleanTests(unittest.TestCase):
         self.assertEqual(SemVer.clean("0.0.0-alpha+a40&/dasdtyh231"), "0.0.0-alpha+a40")
 
     def test_before_and_after(self):
-        self.assertEqual(SemVer.clean(" 123 asda3245 v0.0.0-alpha+a40&!/(§& 23421 sdfa1"), "v0.0.0-alpha+a40")
+        self.assertEqual(SemVer.clean(" 123 asda3245 v0.0.0-alpha+a40&!/(& 23421 sdfa1"), "v0.0.0-alpha+a40")
 
 
 class CoercionTests(unittest.TestCase):
 
-    def test_invalid(self):
-        self.assertTrue(bool(SemVer("0.0.0-a40+alpha")))
-        self.assertFalse(bool(SemVer()))
+    def test_truthiness(self):
+        self.assertTrue(SemVer("0.0.0-a40+alpha"))
+        self.assertFalse(SemVer())
+        v = SemVer()
+        self.assertFalse(v)
+        v.parse("0.0.0")
+        self.assertTrue(v)
+
+    @unittest.skipUnless(sys.version_info[0] == 3, "Only run these in Python 3")
+    def test_compare_string(self):
+        self.assertRaises(TypeError, lambda: SemVer("0.0.0-beta") > "0.0.0-alpha")
+
+    @unittest.skipUnless(sys.version_info[0] == 2, "Only run these in Python 2")
+    def test_unicode(self):
+        # u'' is a syntax error in Py3 so wrap it in eval() (better approaches appreciated)
+        self.assertTrue(eval('SemVer.valid(u"0.0.0")'))
+
+    def test_to_string(self):
+        self.assertEqual(str(SemVer("0.0.0-beta")), "0.0.0-beta")
 
 
 class RangeChecks(unittest.TestCase):
